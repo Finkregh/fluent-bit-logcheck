29207dbd5c9baa24dd41567f1dc1934a4f0ec2ca
Finkregh <Finkregh@users.noreply.github.com>
2026-02-14 10:57:29 +0000

    Deploy preview for PR 1 🛫

---
+++ pr-preview/pr-1/logcheck_fluent_bit_filter/rules/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["CompiledRules","RuleCategory","RuleError"],"struct":["LogcheckDatabase","RuleSet"]};
---
+++ pr-preview/pr-1/trait.impl/serde_core/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["logcheck_fluent_bit_filter",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"logcheck_fluent_bit_filter/rules/enum.RuleCategory.html\" title=\"enum logcheck_fluent_bit_filter::rules::RuleCategory\">RuleCategory</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"logcheck_fluent_bit_filter/cli/output/struct.FilteredEntry.html\" title=\"struct logcheck_fluent_bit_filter::cli::output::FilteredEntry\">FilteredEntry</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"logcheck_fluent_bit_filter/struct.LogEntry.html\" title=\"struct logcheck_fluent_bit_filter::LogEntry\">LogEntry</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1029]}
