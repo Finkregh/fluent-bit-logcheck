f73e7531785cd164bbd02bb7b34d98facd6d2ecb
Finkregh <Finkregh@users.noreply.github.com>
2026-02-16 21:20:37 +0000

    Deploy preview for PR 10 🛫

---
+++ pr-preview/pr-10/trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["logcheck_fluent_bit_filter",[["impl Subcommand for <a class=\"enum\" href=\"logcheck_fluent_bit_filter/cli/args/enum.InputSource.html\" title=\"enum logcheck_fluent_bit_filter::cli::args::InputSource\">InputSource</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[223]}
---
+++ pr-preview/pr-10/trait.impl/core/cmp/trait.PartialEq.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["logcheck_fluent_bit_filter",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"logcheck_fluent_bit_filter/rules/enum.RuleCategory.html\" title=\"enum logcheck_fluent_bit_filter::rules::RuleCategory\">RuleCategory</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[350]}
